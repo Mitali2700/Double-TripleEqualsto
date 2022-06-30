76d33dfe37b2fce1c1ac9cf64d6983c2ed84b667
Mitali Gupta <78539161+Mitali2700@users.noreply.github.com>
2022-06-30 15:57:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var a = 1;
+var b='1';
+
+//Three Equal Signs
+if(a===b){
+  console.log("True");
+}
+else{
+  console.log("False");
+}
+
+
+// Two Equal Signs
+if(a===b){
+  console.log("Right");
+}
+else{
+  console.log("Wrong");
+}
+
+//The important difference between three equal signs and two equal signs, even thought they both checks for equality, with three equal signs it's also checked to see that the data types are matching, whereas with two equal signs it doesn't care about the data types.
